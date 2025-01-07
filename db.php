0dbd72aa93dc92fd28fd373b9dc0a78c19258ee6
Sam Muttai <muttaisammy@gmail.com>
2025-01-07 17:27:19 +0300

    add

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$db = "kenstate_onlineshop";
+$db = "onlineshop";
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password,$db);
